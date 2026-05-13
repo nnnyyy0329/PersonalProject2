9a14e8001cf4b3f0fc703419c8c45154d1022428
nnnyyy <125cr0003@tokyo.amg.ac.jp>
2026-05-13 19:48:43 +0900

    ModeGame少し変更

--- Game/Game/source/ModeGame.h
+++ Game/Game/source/ModeGame.h
@@ -1,14 +1,19 @@
 #include "appframe.h"
 
+/// @brief ゲームモードクラス
 class ModeGame : public ModeBase
 {
 	typedef ModeBase base;
 public:
-	virtual bool Initialize();
-	virtual bool Terminate();
-	virtual bool Process();
-	virtual bool Render();
+
+	/* 基本関数 */
+	virtual bool Initialize();	/// @brief 初期化関数
+	virtual bool Terminate();	/// @brief 終了関数
+	virtual bool Process();		/// @brief 更新関数
+	virtual bool Render();		/// @brief 描画関数
 
 protected:
+
+
 
 }; 
