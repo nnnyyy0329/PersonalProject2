b8705f612b74e9bcfb569a5332428fbc5f03328c
nnnyyy <125cr0003@tokyo.amg.ac.jp>
2026-05-15 11:52:49 +0900

    カメラクラス .h 内のコーディング変更
    今後、ゲームカメラ.hのコーディングをベースに作っていく

--- Game/Game/source/GameCamera.cpp
+++ Game/Game/source/GameCamera.cpp
@@ -1,8 +1,24 @@
 #include "GameCamera.h"
 
+namespace
+{
+	/// @brief 初期カメラ設定
+	namespace Default
+	{
+		const VECTOR CAMERA_POS = VGet(73.636536f, 86.688026f, -140.440582f);
+		const VECTOR CAMERA_TARGET = VGet(-12.230986f, 59.101776f, -15.002045f);
+
+		//const VECTOR CAMERA_POS = { 0.0f, 10.0f, 0.0f };	/// @brief カメラの初期位置
+		//const VECTOR CAMERA_TARGET = { 0.0f, 0.0f, 0.0f };	/// @brief カメラの初期注視点
+		const VECTOR CAMERA_UPVEC = { 0.0f, 1.0f, 0.0f };	/// @brief カメラの初期アップベクトル
+	}
+}
+
 GameCamera::GameCamera()
 {
-
+	m_cameraData.pos	= Default::CAMERA_POS;
+	m_cameraData.target = Default::CAMERA_TARGET;
+	m_cameraData.upVec	= Default::CAMERA_UPVEC;
 }
 
 GameCamera::~GameCamera()
@@ -12,9 +28,6 @@ GameCamera::~GameCamera()
 
 bool GameCamera::Initialize()
 {
-	m_clipNear	= 2.0f;
-	m_clipFar	= 10000.0f;
-
 	return true;
 }
 
@@ -25,11 +38,18 @@ bool GameCamera::Terminate()
 
 bool GameCamera::Process()
 {
+	auto offset = VGet(0.0f, 10.0f, -20.0f);
+	m_cameraData.pos.y += offset.y;
+
 	return true;
 }
 
 bool GameCamera::SetUpCamera() const
 {
+	/*SetCameraPositionAndTarget_UpVecY(VGet(73.636536f, 86.688026f, -140.440582f), VGet(-12.230986f, 59.101776f, -15.002045f));
+	SetCameraNearFar(2.376863f, 594.215820f);*/
+
+
 	// カメラの位置と注視点を設定
 	SetCameraPositionAndTarget_UpVecY(m_cameraData.pos, m_cameraData.target);
 
