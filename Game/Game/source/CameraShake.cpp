610b13affc7efa1a46721254a3765309943f812d
nnnyyy <125cr0003@tokyo.amg.ac.jp>
2026-06-29 20:41:03 +0900

    カメラ振動クラスを追加
    CameraShake:
    PlayShakeで与えられた値を使い、ランダムな範囲で振動させるクラス。
    改善点がおおく、更新処理の呼び出しや、カメラ振動クラスのポインタなどをどうほかの部分で取得して使うかはまだ決まっていない。
    こことは別で、無理やり入れようとしたが、依存関係やもともとの設計を無視した作り方をして、せっかくの一貫性や可読性などを損なってしまったのでいったん断念。
    カメラクラスは、全体的にコンポーネント化をしようと考えているので、カメラ振動のコンポーネントなども今後作りたいと考えている。

---
+++ Game/Game/source/CameraShake.cpp
@@ -0,0 +1,59 @@
+#include "CameraShake.h"
+
+namespace
+{
+	// ランダム値の最大値
+	constexpr int MAX_RAND = 2000;
+
+	// ランダム値の半分の値
+	constexpr int HALF_MAX_RAND = MAX_RAND / 2;
+
+	// ランダム値のスケール
+	constexpr float RANDOM_SCALE = 1000.0f;
+}
+
+void CameraShake::Update()
+{
+	// タイマーが動いていなければ何もしない
+	if(m_shakeTimer <= 0.0f)
+	{
+		// 振動オフセットをリセット
+		m_shakeOffset = VGet(0.0f, 0.0f, 0.0f);
+
+		return;
+	}
+
+	// タイマーを減らす
+	m_shakeTimer -= 1.0f;
+
+	// 時間切れになったら即終了
+	if(m_shakeTimer <= 0.0f)
+	{
+		// 振動オフセットをリセット
+		m_shakeOffset = VGet(0.0f, 0.0f, 0.0f);
+
+		return;
+	}
+
+	// 残り時間の割合
+	float rate = m_shakeTimer / m_duration;
+
+	// 振動の強さを計算
+	float currentMagnitude = m_magnitude * rate;
+
+	// ランダムな振動オフセットを計算
+	float randX = ((GetRand(MAX_RAND) - HALF_MAX_RAND) / RANDOM_SCALE) * currentMagnitude;
+	float randY = ((GetRand(MAX_RAND) - HALF_MAX_RAND) / RANDOM_SCALE) * currentMagnitude;
+	float randZ = ((GetRand(MAX_RAND) - HALF_MAX_RAND) / RANDOM_SCALE) * currentMagnitude;
+
+	// 振動オフセットを更新
+	m_shakeOffset = VGet(randX, randY, randZ);
+}
+
+void CameraShake::PlayShake(float duration, float magnitude)
+{
+	m_shakeTimer	= duration;
+	m_duration		= duration;
+	m_magnitude		= magnitude;
+	m_shakeOffset	= { 0.0f, 0.0f, 0.0f };
+}
