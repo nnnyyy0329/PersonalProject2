148e9980905a8e9ee6b19beee575c9cc3151806f
nnnyyy <125cr0003@tokyo.amg.ac.jp>
2026-07-27 16:33:04 +0900

    マップのコリジョン表示切替（バグあり）
    マップの床にあったコリジョンの表示を切り替えられるように。
    マップクラスに更新関数を追加して、そこに表示切替のラムダ式を書いた。
    
    デバッグモードを切り替えてコリジョン表示をなくしても、正常に戻らずモード切り替え時に一瞬だけマップコリジョンが出現するというようになってしまった。

--- Game/Game/source/Map.cpp
+++ Game/Game/source/Map.cpp
@@ -1,6 +1,7 @@
 #include "Map.h"
 #include "Server/ResourceServer.h"
 #include "Server/SoundServer.h"
+#include "DebugManager.h"
 
 bool Map::Initialize()
 {
@@ -11,14 +12,11 @@ bool Map::Initialize()
 	m_mapData.mapHandle = rs->GetHandle("Map");
 	m_mapData.skyHandle = rs->GetHandle("Sky");
 
-
-
+	// マップのコリジョンハンドル取得
+	m_mapCollisionHandle = MV1SearchFrame(m_mapData.mapHandle, "dungeon_collision");
 
 	// ステージBGMのハンドル取得、再生
 	m_stageBgm = SoundServer::GetInstance()->Play("BGM_Stage", DX_PLAYTYPE_BACK);
-
-
-
 
 	return true;
 }
@@ -28,6 +26,35 @@ bool Map::Terminate()
 	// グラフィックハンドル解放
 	MV1DeleteModel(m_mapData.mapHandle);
 	MV1DeleteModel(m_mapData.skyHandle);
+	
+	// コリジョンハンドル解放
+	MV1DeleteModel(m_mapCollisionHandle);
 
 	return true;
+}
+
+void Map::Update()
+{
+	// デバッグクラスのインスタンスを取得
+	auto& debugManager = DebugManager::GetInstance();
+	auto& debugCollision = debugManager.GetDebugCollision();
+
+	// デバッグモードの場合
+	if(debugManager.IsDebugMode())
+	{
+		// コリジョンを表示
+		debugCollision.AddColItem([this]()
+			{
+				MV1SetFrameVisible(m_mapData.mapHandle, m_mapCollisionHandle, FALSE);
+			});
+	}
+	// デバッグモードでない場合
+	else
+	{
+		// コリジョンを非表示
+		debugCollision.AddColItem([this]()
+			{
+				MV1SetFrameVisible(m_mapData.mapHandle, m_mapCollisionHandle, TRUE);
+			});
+	}
 }
